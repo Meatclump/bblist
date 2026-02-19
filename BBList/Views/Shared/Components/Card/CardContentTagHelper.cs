ddb322de1f6d587416a606c7e3f95116a4f2ecd8
Meatclump <hepe22@hotmail.com>
2026-02-19 13:35:45 +0100

    Added CardContent component

--- BBList/Views/Shared/Components/Card/CardContentTagHelper.cs
+++ BBList/Views/Shared/Components/Card/CardContentTagHelper.cs
@@ -1,20 +1,18 @@
 ﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using System.Drawing;
 
 namespace BBList.Views.Shared.Components.Card
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
-    //[HtmlTargetElement("card")]
-    public class CardTagHelper : TagHelper
+    //[HtmlTargetElement("tag-name")]
+    public class CardContentTagHelper : TagHelper
     {
         public string Size { get; set; } = "default";
-
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "div";
             output.Attributes.SetAttribute("data-size", Size);
-            output.Attributes.SetAttribute("class", "ring-slate-900/10 bg-card text-slate-900 gap-4 overflow-hidden rounded-xl py-4 text-sm ring-1 has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl group/card flex flex-col");
+            output.Attributes.SetAttribute("class", "px-4 group-data-[size=sm]/card:px-3");
         }
     }
 }
