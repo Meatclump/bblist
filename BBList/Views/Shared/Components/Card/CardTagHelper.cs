133d7dbf18e00a94b384d3a1232041f0fdcf45f6
Meatclump <hepe22@hotmail.com>
2026-02-19 11:41:54 +0100

    Set up tag helpers for Identity pages

--- BBList/Views/Shared/Components/Card/CardTagHelper.cs
+++ BBList/Views/Shared/Components/Card/CardTagHelper.cs
@@ -4,12 +4,13 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace BBList.Views.Shared.Components.Card
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
-    [HtmlTargetElement("tag-name")]
+    //[HtmlTargetElement("card")]
     public class CardTagHelper : TagHelper
     {
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "ring-foreground/10 bg-card text-card-foreground gap-4 overflow-hidden rounded-xl py-4 text-sm ring-1 has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl group/card flex flex-col");
         }
     }
 }
