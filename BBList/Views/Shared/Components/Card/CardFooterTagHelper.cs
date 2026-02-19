259deb98e9ef27894e3bd3d491c8cdb94912a45c
Meatclump <hepe22@hotmail.com>
2026-02-19 13:48:24 +0100

    Added CardFooter component

---
+++ BBList/Views/Shared/Components/Card/CardFooterTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BBList.Views.Shared.Components.Card
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    //[HtmlTargetElement("tag-name")]
+    public class CardFooterTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "bg-slate-300/50 rounded-b-xl border-t border-slate-300/50 p-4 group-data-[size=sm]/card:p-3 flex items-center");
+        }
+    }
+}
