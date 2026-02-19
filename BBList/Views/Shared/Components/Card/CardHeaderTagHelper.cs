0a95bbc88598742280a519659943548b808426c7
Meatclump <hepe22@hotmail.com>
2026-02-19 13:42:53 +0100

    Added CardTitle component

---
+++ BBList/Views/Shared/Components/Card/CardHeaderTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BBList.Views.Shared.Components.Card
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    //[HtmlTargetElement("tag-name")]
+    public class CardHeaderTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "gap-1 rounded-t-xl px-4 group-data-[size=sm]/card:px-3 [.border-b]:pb-4 group-data-[size=sm]/card:[.border-b]:pb-3 group/card-header @container/card-header grid auto-rows-min items-start has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto]");
+        }
+    }
+}
--- BBList/Views/Shared/Components/Card/CardTitleTagHelper.cs
+++ BBList/Views/Shared/Components/Card/CardTitleTagHelper.cs
@@ -5,12 +5,12 @@ namespace BBList.Views.Shared.Components.Card
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
     //[HtmlTargetElement("tag-name")]
-    public class CardHeader : TagHelper
+    public class CardTitleTagHelper : TagHelper
     {
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "div";
-            output.Attributes.SetAttribute("class", "gap-1 rounded-t-xl px-4 group-data-[size=sm]/card:px-3 [.border-b]:pb-4 group-data-[size=sm]/card:[.border-b]:pb-3 group/card-header @container/card-header grid auto-rows-min items-start has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto]");
+            output.Attributes.SetAttribute("class", "text-base leading-snug font-medium group-data-[size=sm]/card:text-sm");
         }
     }
 }
