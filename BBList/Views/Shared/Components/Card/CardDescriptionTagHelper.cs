ffacd63e29246abb8628835c0167218fc8055a10
Meatclump <hepe22@hotmail.com>
2026-02-19 13:45:09 +0100

    Added CardDescription component

---
+++ BBList/Views/Shared/Components/Card/CardDescriptionTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BBList.Views.Shared.Components.Card
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    //[HtmlTargetElement("tag-name")]
+    public class CardDescriptionTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "text-slate-900/50 text-sm");
+        }
+    }
+}
