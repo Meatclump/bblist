d61d968ce15187e9420672241c33258b5eed7981
Meatclump <hepe22@hotmail.com>
2026-02-19 14:19:42 +0100

    Added CardAction component

---
+++ BBList/Views/Shared/Components/Card/CardActionTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BBList.Views.Shared.Components.Card
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    //[HtmlTargetElement("tag-name")]
+    public class CardActionTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("data-slot", "card-action");
+            output.Attributes.SetAttribute("class", "col-start-2 row-span-2 row-start-1 self-start justify-self-end");
+        }
+    }
+}
